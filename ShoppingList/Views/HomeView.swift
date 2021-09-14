baa2b481ef2a1ed0e356aaca343fceab096ddfe7
Javid Museyibli <javid.museyibli@pashabank.az>
2021-09-14 23:15:14 +0400

    SL-1: Restructure project files

--- ShoppingList/Views/HomeView.swift
+++ ShoppingList/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  ShoppingList
 //
 //  Created by Javid Museyibli on 14.09.21.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
