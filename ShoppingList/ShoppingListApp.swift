f82429c7f2dee7a57d3c0059332d393d9ccbf25c
Javid Museyibli <javid.museyibli@pashabank.az>
2021-09-14 23:11:18 +0400

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Javid Museyibli on 14.09.21.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
